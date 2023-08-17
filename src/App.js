23c3de895fe78e11f2c705fecea9718e73c18851
goord <G.vandenOord@gmail.com>
2023-08-17 11:44:45 +0200

    cleanup of App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,6 @@
-import { ImprovedNoise } from 'https://unpkg.com/three/examples/jsm/math/ImprovedNoise.js';
 import { Vol3dViewer } from '@janelia/web-vol-viewer';
 import './App.css';
-import * as THREE from 'three';
-import { openArray, HTTPStore } from 'zarr'
-import React, { useState, useEffect, useRef } from 'react';
+import React from 'react';
 import CloudViewerUI from './CloudViewerUI.jsx';
 
 
