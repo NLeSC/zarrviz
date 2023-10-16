07f3da28e3b94ffc12a921b73d83d6b533d150f7
Jesús García <ctw@ctwhome.com>
2023-10-16 15:38:58 +0200

    support glsl with vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,12 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import viteTsconfigPaths from "vite-tsconfig-paths";
+import glsl from "vite-plugin-glsl";
 
 export default defineConfig({
   // depending on your application, base can also be "/"
   base: "",
-  plugins: [react(), viteTsconfigPaths()],
+  plugins: [react(), viteTsconfigPaths(), glsl()],
   server: {
     // this ensures that the browser opens upon server start
     open: true,
