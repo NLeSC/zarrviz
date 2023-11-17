8c140fa2d9afe7e98368d0803b3d3c5f15cbfed2
Jesús García <ctw@ctwhome.com>
2023-11-17 23:49:57 +0100

    Update server configuration to disable automatic browser opening

--- vite.config.ts
+++ vite.config.ts
@@ -9,7 +9,7 @@ export default defineConfig({
   plugins: [react(), viteTsconfigPaths(), glsl()],
   server: {
     // this ensures that the browser opens upon server start
-    open: true,
+    open: false,
     // this sets a default port to 3000
     port: 3000,
   },
