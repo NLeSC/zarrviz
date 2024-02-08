181ab7dc2b811769e4692490bbc5924a8210d93e
ctwhome <ctw@ctwhome.com>
2024-02-08 14:43:30 +0100

    Update dependencies and layout styles

--- src/routes/viewer/+page.svelte
+++ src/routes/viewer/+page.svelte
@@ -9,23 +9,88 @@
 
 <!--  Debugging info -->
 
-<div class="">
+<div class="grid grid-cols-[1fr_300px] grid-rows-1 w-screen h-screen">
 	<!-- <a class="btn" href="/zarrExample">Zarr playground</a> -->
 	<!-- <pre>Slices downloaded: {@JSON.stringify(allTimeSlices.length, null, 2)}</pre> -->
-	<div class="flex gap-5">
-		<!-- 1073741824 = 1GB -->
-		<pre>dataUint8 (slice) {$allTimeSlices[0]?.length} - {($allTimeSlices[0]?.byteLength / 1073741824).toFixed(
-				3
-			)} GB |</pre>
-		<!-- <pre>dataCellSize: {$dataCellSize.length} |</pre> -->
-		<pre>Slices downloaded: {JSON.stringify($allTimeSlices.length, null, 2)} </pre>
-	</div>
-	<!-- datashape = dataShape: {JSON.stringify($dataShape, null, 2)} -->
-</div>
+	<div class="flex flex-col">
+		<div class="flex gap-5">
+			<!-- 1073741824 = 1GB -->
+			<pre>dataUint8 (slice) {$allTimeSlices[0]?.length} - {($allTimeSlices[0]?.byteLength / 1073741824).toFixed(
+					3
+				)} GB |</pre>
+			<!-- <pre>dataCellSize: {$dataCellSize.length} |</pre> -->
+			<pre>Slices downloaded: {JSON.stringify($allTimeSlices.length, null, 2)} </pre>
+		</div>
+		<!-- datashape = dataShape: {JSON.stringify($dataShape, null, 2)} -->
 
-<Viewer />
-<TimeLine
-	positionIndex={$currentTimeIndex}
-	length={$allTimeSlices.length}
-	on:onSelectedIndex={(value) => currentTimeIndex.set(value.detail.index)}
-/>
+		<!-- <Viewer /> -->
+		<div class="bg-green-900 w-full h-full">viewe here</div>
+		<TimeLine
+			positionIndex={$currentTimeIndex}
+			length={$allTimeSlices.length}
+			on:onSelectedIndex={(value) => currentTimeIndex.set(value.detail.index)}
+		/>
+	</div>
+	<div class="px-4 py-4">
+		<h2 class="text-2xl">Scene</h2>
+		<p>Details</p>
+
+		<h3 class="mt-10 text-xl">Layers</h3>
+
+		<div class="form-control">
+			<label class="label cursor-pointer justify-start gap-3">
+				<input type="checkbox" checked="checked" class="checkbox checkbox-sm" />
+				<span class="label-text w-48">Clouds</span>
+				<input type="range" min="0" max="100" value="40" class="range range-xs" />
+			</label>
+
+			<label class="label cursor-pointer justify-start gap-3">
+				<input type="checkbox" checked="checked" class="checkbox checkbox-sm" />
+				<span class="label-text w-48">Rain</span>
+				<input type="range" min="0" max="100" value="40" class="range range-xs" />
+			</label>
+
+			<label class="label cursor-pointer justify-start gap-3">
+				<input type="checkbox" checked="checked" class="checkbox checkbox-sm" />
+				<span class="label-text w-48">Surface Temperature</span>
+				<input type="range" min="0" max="100" value="40" class="range range-xs" />
+			</label>
+		</div>
+
+		<div class="mt-10 text-xl">Instruments</div>
+		<div class="flex flex-col gap-4 mt-5">
+			<!-- You can open the modal using ID.showModal() method -->
+			<button class="btn" onclick="my_modal_1.showModal()">Instrument 1 details</button>
+			<dialog id="my_modal_1" class="modal">
+				<div class="modal-box w-11/12 max-w-full h-[90%] max-h-full">
+					<form method="dialog">
+						<button class="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">✕</button>
+					</form>
+					<h3 class="font-bold text-lg">Instrument</h3>
+					<p class="py-4">Press ESC key or click on ✕ button to close</p>
+					<iframe
+						class="w-full h-[90%]"
+						src="http://wind-experiment.ceg.tudelft.nl/dataBrowser/dataBrowser4.html?site=CMTRACE-2023&date=2023-06-27&UpperLeft=WC_KNMI_overview&UpperRight=skiron_wind_direction&LowerRight=WC_TUD_wind_speed&LowerLeft=ceilometer"
+						frameborder="0"
+					></iframe>
+				</div>
+			</dialog>
+
+			<button class="btn" onclick="my_modal_2.showModal()">Instrument 2 details</button>
+			<dialog id="my_modal_2" class="modal">
+				<div class="modal-box w-11/12 max-w-full h-[90%] max-h-full">
+					<form method="dialog">
+						<button class="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">✕</button>
+					</form>
+					<h3 class="font-bold text-lg">Instrument</h3>
+					<p class="py-4">Press ESC key or click on ✕ button to close</p>
+					<iframe
+						class="w-full h-[90%]"
+						src="http://wind-experiment.ceg.tudelft.nl/dataBrowser/dataBrowser4.html?site=CMTRACE-2023&date=2023-06-27&UpperLeft=WC_KNMI_overview&UpperRight=skiron_wind_direction&LowerRight=WC_TUD_wind_speed&LowerLeft=ceilometer"
+						frameborder="0"
+					></iframe>
+				</div>
+			</dialog>
+		</div>
+	</div>
+</div>
