bd48e55cc30eb056c4fbcd3e3359d9221922c1ef
Jesús García <ctw@ctwhome.com>
2024-03-06 08:07:03 +0100

    add statsjs frames per second

--- src/lib/components/Stats.svelte
+++ src/lib/components/Stats.svelte
@@ -2,7 +2,7 @@
 	import { onMount } from 'svelte';
 	import Stats from 'stats.js';
 
-	let className = undefined; // 'class' is a reserved keyword in JS, with initialization
+	let className = ''; // 'class' is a reserved keyword in JS, with initialization
 	export { className as class };
 
 	let statsElement: HTMLElement; // This is where you'll append the stats panel
@@ -10,6 +10,14 @@
 	onMount(() => {
 		const stats = new Stats();
 		stats.showPanel(0); // 0: fps, 1: ms, 2: mb, 3+: custom
+
+		// Apply styles directly to stats.dom to position it
+		stats.dom.style.position = 'fixed';
+		stats.dom.style.bottom = '10px';
+		stats.dom.style.right = '10px';
+		stats.dom.style.left = 'auto'; // Override default left positioning
+		stats.dom.style.top = 'auto'; // Ensure top is not affecting the positioning
+
 		statsElement.appendChild(stats.dom); // Append to the bound element instead of body
 
 		function animate() {
@@ -21,14 +29,7 @@
 		}
 
 		requestAnimationFrame(animate);
-
-		return () => {
-			// Cleanup function to remove the stats panel when the component is destroyed
-			if (statsElement && statsElement.contains(stats.dom)) {
-				statsElement.removeChild(stats.dom);
-			}
-		};
 	});
 </script>
 
-<div bind:this={statsElement} class={'z-10 ' + className} />
+<div class={'z-10 fixed bottom-0 right-0 ' + className} bind:this={statsElement}></div>
